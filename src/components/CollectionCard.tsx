c56f293b0da8bd8a02fd1db7e4a38c5014d3acd1
Raheel Junaid <raheeljunaid@Raheels-Mac-Studio.local>
2022-08-14 14:40:46 -0600

    Make Collection Card

--- src/components/CollectionCard.tsx
+++ src/components/CollectionCard.tsx
@@ -1,26 +1,106 @@
 import Marquee from "react-fast-marquee";
+import { trpc } from "../utils/trpc";
+import Image from "next/image";
+import Rating from "./Rating";
+import Link from "next/link";
+import HeartOutline from "../../public/heart-outline.svg";
+import HeartFilled from "../../public/heart-filled.svg";
 
 interface CollectionCardProps {
-  bookCovers: string[];
+  bookCovers: (string | null)[];
   title: string;
-  author: string;
-  profilePic?: string;
+  user: {
+    id: string;
+    name: string;
+    image: string | null;
+  };
   collectionId: string;
-  favouritedByUser: boolean;
+  avgRating: number;
 }
 
 export const CollectionCard = ({
   bookCovers,
   title,
-  author,
-  profilePic,
+  avgRating,
+  user,
   collectionId,
-  favouritedByUser,
 }: CollectionCardProps) => {
+  const utils = trpc.useContext();
+  const { data: isFavourited, status: favouriteStatus } = trpc.useQuery([
+    "collection.isFavourited",
+    { collectionId },
+  ]);
+  const favouriteMutation = trpc.useMutation(["collection.toggleFavourite"], {
+    onMutate() {
+      utils.setQueryData(
+        ["collection.isFavourited", { collectionId }],
+        !isFavourited
+      );
+    },
+    onSettled() {
+      utils.invalidateQueries(["collection.isFavourited", { collectionId }]);
+    },
+  });
+
   return (
-    <div className="grid grid-rows-2 justify-items-stretch">
-      <div></div>
-      <div></div>
+    <div className="flex flex-col shadow-lg shadow-gray-200 border border-t-0 border-gray-200 relative">
+      <Marquee pauseOnHover gradientColor={[229, 231, 235]} gradientWidth={100}>
+        {bookCovers.map((cover, index) => (
+          <div className="shadow-md flex-none w-16 h-24 relative" key={index}>
+            <Image
+              src={(cover as string) || ""}
+              layout="fill"
+              objectFit="cover"
+              objectPosition="center"
+              alt={title}
+            />
+          </div>
+        ))}
+      </Marquee>
+      <span className="flex z-10 top-2 right-2 items-center px-2.5 py-0.5 rounded-full text-xs shadow-md bg-gray-900/75 text-white absolute">
+        {bookCovers.length} Books
+      </span>
+      <div className="p-4 space-y-2">
+        <div className="flex items-center gap-3">
+          <div className="flex-none w-10 aspect-square overflow-hidden rounded-full relative">
+            <Image
+              src={(user.image as string) || ""}
+              layout="fill"
+              objectPosition="center"
+              objectFit="cover"
+              alt={title}
+            />
+          </div>
+          <div>
+            <h2 className="font-medium text-gray-900 line-clamp-1">{title}</h2>
+            <p className="text-gray-500 text-sm">{user.name}</p>
+          </div>
+        </div>
+        <div className="flex justify-between items-center">
+          {avgRating ? (
+            <Rating ratingValue={avgRating} />
+          ) : (
+            <p className="text-gray-300">No Ratings Yet</p>
+          )}
+          <div className="flex items-center gap-3">
+            <Link href={`/collection/${collectionId}`} passHref>
+              <a className="font-medium underline text-purple-600">View Full</a>
+            </Link>
+            {favouriteStatus === "success" && (
+              <Image
+                src={isFavourited ? HeartFilled : HeartOutline}
+                onClick={() => {
+                  favouriteMutation.mutate({ collectionId });
+                }}
+                width={20}
+                height={20}
+                className="cursor-pointer"
+                alt="Favourite"
+              />
+            )}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
